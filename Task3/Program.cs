70b29b8fa91b687c9897a54a5603c1c25e8f4a9b
Vyacheslav <vmk_86@mail.ru>
2022-08-08 13:33:15 +0300

    save5

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,11 +3,11 @@
 Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if (a % 2 == 1)
+if (a % 2 == 0)
 {
-    Console.WriteLine("Число"  + a + "является: НЕЧЁТНЫМ");
+    Console.WriteLine("Число"  + a + "является: ЧЁТНЫМ");
 }
 else
 {
-    Console.WriteLine("Число" + a + "является: ЧЁТНЫМ");
+    Console.WriteLine("Число" + a + "является: НЕЧЁТНЫМ");
 }           
